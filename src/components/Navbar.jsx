d29b712d524e2a9735d296b00c9e35c01cca8e95
Emilie Pessoa Rodrigues <emiliepessoarodrigues@gmail.com>
2025-10-29 22:06:07 -0300

    Adicionando components NavBar & SideBar, e alterando o index

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+// import "./Navbar.css";
+// import logo from "../assets/logo2.svg";
+
+// function Navbar() {
+//   return (
+//     <nav className="navbar">
+//       <Link to="/" className="navbar-logo">
+//         <img src={logo} alt="Logo" />
+//       </Link>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
