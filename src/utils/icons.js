842a0760858dad1db50aeebce0ad2ccde349dffb
Emilie Pessoa Rodrigues <emiliepessoarodrigues@gmail.com>
2025-11-01 16:29:18 -0300

    Criando um arquivo separado de ícones, para não ter que ficar com 500 importações em cada arquivo

---
+++ src/utils/icons.js
@@ -0,0 +1,23 @@
+// REFERENTES A SIDEBAR
+import homeIcon from "../assets/sidebar/icon-home.svg";
+import ingressoIcon from "../assets/sidebar/icon-ingresso.svg";
+import certificadoIcon from "../assets/sidebar/icon-certificado.svg";
+import pesquisasIcon from "../assets/sidebar/icon-pesquisas.svg";
+import calendarioIcon from "../assets/sidebar/icon-calendario.svg";
+import dadosIcon from "../assets/sidebar/icon-dados.svg";
+import comemoracaoIcon from "../assets/sidebar/icon-comemoracao.svg";
+import perfilIcon from "../assets/sidebar/icon-microfone.svg";
+import setaIcon from "../assets/sidebar/icon-seta.svg";
+
+export const icons = {
+    // SIDEBAR
+    home: homeIcon,
+    ingresso: ingressoIcon,
+    certificado: certificadoIcon,
+    pesquisas: pesquisasIcon,
+    calendario: calendarioIcon,
+    dados: dadosIcon,
+    comemoracao: comemoracaoIcon,
+    perfil: perfilIcon,
+    seta: setaIcon,
+};
