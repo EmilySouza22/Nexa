e9d4221200aa647f2c06c728d21c6c6d26113917
francagiovanna <francamariagiovanna4@gmail.com>
2025-11-11 20:43:07 -0300

    WIP: div tela principal feita

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import HomeOrganizador from "./pages/home-organizador/HomeOrganizador";
+import CriacaoEventos from "./pages/criacao-evento/CriacaoEvento"
 
 
 function App() {
   return <HomeOrganizador />;
+  return <CriacaoEventos />;
 }
 
 export default App;
---
+++ src/pages/criacao-evento/CriacaoEvento.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CriacaoEvento() {
+  return (
+    <div className='box-01'>
+        conteúdo do box
+    </div>
+  )
+}
+
+
+export default CriacaoEvento
