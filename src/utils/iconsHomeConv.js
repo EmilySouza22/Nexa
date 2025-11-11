583dc1c43c682445b10eb58e44e8ec2708e340ad
EmilySouza22 <emilyseafonso@gmail.com>
2025-11-11 17:24:45 -0300

    Organizando icons

--- src/utils/iconsHomeConv.js
+++ src/utils/iconsHomeConv.js
@@ -1,17 +1,34 @@
-import calendarioIcon from '..\assets\home-convidado\icon-calendario.svg';
-import certificadoIcon from "..\assets\home-convidado\icon-certificado.svg";
-import discoIcon from "..\assets\home-convidado\icon-disco.svg";
-import esportesIcon from "..\assets\home-convidado\icon-esportes.svg";
-import facebookIcon from "..\assets\home-convidado\icon-facebook.svg";
-import gastronomiaIcon from "..\assets\home-convidado\icon-gastronomia.svg";
-import horaIcon from "..\assets\home-convidado\icon-hora.svg";
-import infantilIcon from "..\assets\home-convidado\icon-infantil.svg";
-import instagramIcon from "..\assets\home-convidado\icon-instagram.svg";
-import linkedinIcon from "..\assets\home-convidado\icon-linkedin.svg";
-import localIcon from "..\assets\home-convidado\icon-local.svg";
-import lupaIcon from "..\assets\home-convidado\icon-lupa.svg";
-import mascaraIcon from "..\assets\home-convidado\icon-mascara.svg";
-import microfoneIcon from "..\assets\home-convidado\icon-microfone.svg";
-import musicIcon from "..\assets\home-convidado\icon-music.svg";
-import reuniaoIcon from "..\assets\home-convidado\icon-reuniao.svg";
-import organizadorIcon from "..\assets\home-organizador";
+import calendarioIcon from '../assets/home-convidado/icon-calendario.svg';
+import certificadoIcon from "../assets/home-convidado/icon-certificado.svg";
+import discoIcon from "../assets/home-convidado/icon-disco.svg";
+import esportesIcon from "../assets/home-convidado/icon-esportes.svg";
+import facebookIcon from "../assets/home-convidado/icon-facebook.svg";
+import gastronomiaIcon from "../assets/home-convidado/icon-gastronomia.svg";
+import horaIcon from "../assets/home-convidado/icon-hora.svg";
+import infantilIcon from "../assets/home-convidado/icon-infantil.svg";
+import instagramIcon from "../assets/home-convidado/icon-instagram.svg";
+import linkedinIcon from "../assets/home-convidado/icon-linkedin.svg";
+import localIcon from "../assets/home-convidado/icon-local.svg";
+import lupaIcon from "../assets/home-convidado/icon-lupa.svg";
+import mascaraIcon from "../assets/home-convidado/icon-mascara.svg";
+import microfoneIcon from "../assets/home-convidado/icon-microfone.svg";
+import musicIcon from "../assets/home-convidado/icon-music.svg";
+import reuniaoIcon from "../assets/home-convidado/icon-reuniao.svg";
+import organizadorIcon from "../assets/home-organizador";
+
+// export const iconsHomeConv = {
+//     home: homeIcon,
+//     ingresso: ingressoIcon,
+//     certificado: certificadoIcon,
+//     pesquisas: pesquisasIcon,
+//     calendario: calendarioIcon,
+//     dados: dadosIcon,
+//     comemoracao: comemoracaoIcon,
+//     microfone: microfoneIcon,
+//     seta: setaIcon,
+//     seta2: setaIcon2,
+//     perfil: perfilIcon,
+//     configuracoes: configuracoesIcon,
+// };
+
+//não repetir nomes de icons de outras telas pq pode interferir
