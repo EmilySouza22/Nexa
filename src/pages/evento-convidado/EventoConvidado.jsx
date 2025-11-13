f17ab2ec67c74e6ddae080625e993722a6d83abf
Alioliv <alyceolievira0717@gmail.com>
2025-11-12 21:06:03 -0300

    adicionei arquivo dos icon

---
+++ src/pages/evento-convidado/EventoConvidado.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'; 
+import "./EventoConvidado.css";
+
+
+
+function EventoConvidado (){
+
+
+    return(
+        <div>
+            <div className='container-compra'>
+                <h1>teste</h1>
+            </div>
+            <div className='conatiner-descricao'>
+                <h1>teste</h1>
+            </div>
+            <div className='container-organizador'>
+                <h1>teste</h1>
+            </div>
+
+
+
+
+
+
+        </div>
+    ); 
+
+}
+
+
+export default EventoConvidado; 
---
+++ src/utils/iconEventoConvidado.js
@@ -0,0 +1,24 @@
+// TELA EVENTO - CONVIDADO 
+
+import classificacaoIcon from "../assets/EventoConvidado/icon-classificacao.svg";
+import dataIcon from "../assets/EventoConvidado/icon-data.svg";
+import descricaoIcon from "../assets/EventoConvidado/icon-descricao.svg";
+import emailIcon from "../assets/EventoConvidado/icon-email.svg";
+import horaIcon from "../assets/EventoConvidado/icon-hora.svg";
+import localCinzaIcon from "../assets/EventoConvidado/icon-local-cinza.svg";
+import localPalataIcon from "../assets/EventoConvidado/icon-local-palata.svg";
+import localMaisEventosIcon from "../assets/EventoConvidado/icon-mais-evento.svg";
+import sobreOrganizadorIcon from "../assets/EventoConvidado/icon-sobre-organizador.svg";
+
+export const icones = {
+    classificacao: classificacaoIcon,
+    data: dataIcon, 
+    descricao: descricaoIcon,
+    email: emailIcon,
+    hora: horaIcon, 
+    local_cinza: localCinzaIcon,
+    local_paleta: localPalataIcon,
+    local_mais_evento: localMaisEventosIcon,
+    sobre_organizador: sobreOrganizadorIcon,
+}
+
