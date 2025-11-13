20f2201c9d69b576b6b2ce1bb9e59ff74064140d
francagiovanna <francagiovanna4@gmail.com>
2025-11-12 21:10:15 -0300

    WIP: criação primeiro box de form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import "./App.css";
 import HomeOrganizador from "./pages/home-organizador/HomeOrganizador";
-import CriacaoEventos from "./pages/criacao-evento/CriacaoEvento"
 
 
 function App() {
-  return (
-    <>
-      <HomeOrganizador />
-      <CriacaoEventos />
-    </>
-  );
+  return <HomeOrganizador />;
+  
 }
 
 export default App;
