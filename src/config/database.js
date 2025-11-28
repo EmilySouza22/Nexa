dae9adc46e11b5c45e21c52b933dd19a996c3989
Emilie Pessoa Rodrigues <emiliepessoarodrigues@gmail.com>
2025-11-27 21:19:18 -0300

    Arrumando a password pois to no computador pessoal

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'senai',
+    password: 'root',
     database: 'nexa',
 
     // Para otimizar perfomance e vitar erros
