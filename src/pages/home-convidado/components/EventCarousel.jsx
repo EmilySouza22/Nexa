8e2cb8b48050cc54e59336fc14b5a861b5be2e49
emilysouza22 <emilyseafonso@gmail.com>
2025-12-04 17:18:41 -0300

    WIP: Implementando carrosel

---
+++ src/pages/home-convidado/components/EventCarousel.jsx
@@ -0,0 +1,41 @@
+import useEmblaCarousel from "embla-carousel-react";
+import { iconsHomeConv } from "../../../utils/iconsHomeConv.js";
+import "./EventCarousel.css"; 
+
+export function EventCarousel() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+
+  const scrollPrev = () => emblaApi && emblaApi.scrollPrev();
+  const scrollNext = () => emblaApi && emblaApi.scrollNext();
+
+  const slides = [
+    { id: 1, title: "1" },
+    { id: 2, title: "2" },
+    { id: 3, title: "3" },
+  ];
+
+  return (
+    <div className="carousel-container">
+      <button className="embla__prev" onClick={scrollPrev}>
+        <img src={iconsHomeConv.setadaEsquerda} className="HomeConv-iconSeta" alt="Seta-Voltar" />
+      </button>
+
+      { /* VIEWPORT */}
+      <div className="embla" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((slide) => (
+            <div className="embla__slide" key={slide.id}>
+              <div className="slide-card">
+                <h3>{slide.title}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <button className="embla__next" onClick={scrollNext}>
+        <img src={iconsHomeConv.setadaDireita} className="HomeConv-iconSeta" alt="Seta-Proximo" />
+      </button>
+    </div>
+  );
+}
